5e1558e676ca7fedd8b7a2a1d37db275475eb82b
Matheus Saboia Raposo <matheus.s.raposo@Gmail.com>
2026-03-19 19:57:44 -0300

    fix: TEA-PIT

--- src/modules/care/index.ts
+++ src/modules/care/index.ts
@@ -8,7 +8,6 @@ import reportTemplateRoutes from './routes/report-templates';
 import reportPhraseRoutes from './routes/report-phrases';
 import reportConfigRoutes from './routes/report-config';
 import reportAddendumRoutes from './routes/report-addendums';
-import envelopmentRoutes from './routes/envelopments';
 import documentRoutes from './routes/documents';
 import teaProfilesRoutes from './routes/tea-profiles';
 import teaPreReservationsRoutes from './routes/tea-pre-reservations';
@@ -27,7 +26,6 @@ export default async function careModule(app: FastifyInstance) {
   app.register(reportPhraseRoutes, { prefix: '/report-phrases' });
   app.register(reportConfigRoutes, { prefix: '/report-config' });
   app.register(reportAddendumRoutes, { prefix: '/report-addendums' });
-  app.register(envelopmentRoutes, { prefix: '/envelopments' });
   app.register(documentRoutes, { prefix: '/documents' });
   app.register(teaProfilesRoutes, { prefix: '/tea-profiles' });
   app.register(teaPreReservationsRoutes, { prefix: '/tea-pre-reservations' });
